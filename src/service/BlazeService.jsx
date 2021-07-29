e02458c8d57dd08c329f48d024fbb54f9a459385
22jair <jair_55_88@hotmail.com>
2021-07-29 14:33:58 -0500

    Blaze FrontEnd

--- src/service/BlazeService.jsx
+++ src/service/BlazeService.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BlazeService = axios.create({
-    // baseURL: "http://localhost:8080/",
-    baseURL: "https://blaze-services.herokuapp.com/",
+    baseURL: "http://localhost:8080/",
+    // baseURL: "https://blaze-services.herokuapp.com/",
 });
 
 export default BlazeService;
